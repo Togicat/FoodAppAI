5569dc33f6b3d72987b99c3d11f922a72ad567da
tenczer.antal@ssakhk.cz <tenczer.antal@ssakhk.cz>
2025-10-20 11:02:55 +0200

    suicide

---
+++ FoodAppAI/Api/GeminiApi.cs
@@ -0,0 +1,6 @@
+namespace FoodAppAI.Api;
+
+public class GeminiApi
+{
+    
+}
---
+++ FoodAppAI/Models/FoodInfo.cs
@@ -0,0 +1,9 @@
+namespace FoodAppAI.Models;
+
+public class FoodInfo
+{
+    public string Name { get; set; }
+    public int Calories { get; set; }
+    public double Protein { get; set; }
+    public double Carbs { get; set; }
+}
